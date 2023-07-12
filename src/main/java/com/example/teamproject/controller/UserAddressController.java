e76db3b5afe1580c2ab989652e2e663530135b19
AZIZBEK <azizbekusmonov2418@gmail.com>
2023-07-12 18:44:45 +0500

    master

--- src/main/java/com/example/teamproject/controller/UserAddressController.java
+++ src/main/java/com/example/teamproject/controller/UserAddressController.java
@@ -48,7 +48,7 @@ public class UserAddressController {
         List<UserAddress> allByUsersId = userAddressRepository.findAllByUsersId(users_id);
         return allByUsersId;
     }
-//11111
+//1111111
     @DeleteMapping(value = "/address/{id}")
     public String deleteUserAddress(@RequestBody UserAddress userAddress, @PathVariable Integer id){
         userAddressRepository.deleteById(id);
